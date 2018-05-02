ef7d6f82ccafdb644e24001827ccb59ab7f38d87
nangelos <nicksangelos@gmail.com>
2018-05-02 14:10:37 -0500

    adding video component

---
+++ src/Components/video/video-screen.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+
+class Screen extends Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0,
+        enablejsapi: 1,
+        modestbranding: 1,
+        //origin: ourdomain.com,
+        //playlist: comma-separated list of video ids,
+        rel: 1,
+      },
+    };
+
+    return (
+      <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />
+    );
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+}
+
+export default Screen;
+
+// // 2. This code loads the IFrame Player API code asynchronously.
+// var tag = document.createElement('script');
+
+// tag.src = 'https://www.youtube.com/iframe_api';
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '390',
+//     width: '640',
+//     videoId: 'M7lc1UVf-VE',
+//     events: {
+//       onReady: onPlayerReady,
+//       onStateChange: onPlayerStateChange,
+//     },
+//   });
+// }
+
+// // 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//   event.target.playVideo();
+// }
+
+// // 5. The API calls this function when the player's state changes.
+// //    The function indicates that when playing a video (state=1),
+// //    the player should play for six seconds and then stop.
+// var done = false;
+// function onPlayerStateChange(event) {
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     setTimeout(stopVideo, 6000);
+//     done = true;
+//   }
+// }
+// function stopVideo() {
+//   player.stopVideo();
+// }
---
+++ src/Components/video/video.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Screen from './video-screen';
+
+class Video extends Component {
+  render() {
+    return (
+      <div>
+        <Screen />
+      </div>
+    );
+  }
+}
+
+export default Video;
