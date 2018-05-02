d988aaf1401921f2f4423f0db471605c04105681
Jaime Syvino <jsyvino@gmail.com>
2018-05-02 13:46:05 -0500

    added basic ugly navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
