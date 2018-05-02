45dbeeb50da5eecab994943d52dc009b86605d04
David Lu <dlu777@gmail.com>
2018-05-02 15:46:05 -0500

    basic chat

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar'
+import {Chat} from './Chat'
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Chat />
       </div>
     );
   }
