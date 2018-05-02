2a7c6aa6f2e515046c147273e7772eab0985f520
David Lu <dlu777@gmail.com>
2018-05-02 16:02:50 -0500

    add firebaseconfig

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBsmlpGgIfYaPyybHZd8m6LH6d2_uI7u0s",
+  authDomain: "wetube-ef349.firebaseapp.com",
+  databaseURL: "https://wetube-ef349.firebaseio.com",
+  projectId: "wetube-ef349",
+  storageBucket: "wetube-ef349.appspot.com",
+  messagingSenderId: "53974512659"
+};
