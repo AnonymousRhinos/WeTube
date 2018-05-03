91c2685955208d8d0aa6365831a655015142025c
Jaime Syvino <jsyvino@gmail.com>
2018-05-03 08:44:02 -0500

    added css framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { withRouter, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar';
 import Video from './Components/video/video';
@@ -12,10 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to WeTube</h1>
-        </header>
         <Switch>
           <Route path="/room/:id" component={Video} />
           <Route path="/home" component={Home} />
