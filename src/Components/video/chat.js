0a5aa3dd0f546081d6123008bda46c626db0ef32
Jvwalsh <jvwalsh@icloud.com>
2018-05-04 09:25:24 -0500

    adding color to DB

--- src/Components/video/chat.js
+++ src/Components/video/chat.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
 import myFirebase from '../../Firebase/firebaseInit';
+import colors from '../../colors.js';
 
 export default class Chat extends Component {
   constructor(props) {
     super(props);
+
+
     this.state = {
       name: '',
       messages: [],
-    };
+      color: ''
+        };
+
   }
 
 
   handleSubmit = event => {
     event.preventDefault();
-    const { name } = this.state;
+    const { name, color } = this.state;
     const messagesRef = myFirebase
       .database()
       .ref('messages/' + this.props.roomId);
     const message = {
       user: name,
       message: event.target.text.value,
+      color: color
     };
     messagesRef.push(message);
+    console.log(this.state, ' this is a color')
+
   };
 
   componentDidMount = () => {
     const name = prompt('Enter name:');
-    this.setState({ name: name });
+    const color = this.establishColor(colors);
+    this.setState({ 
+      name: name,
+      color: color 
+    });
     const messagesRef = myFirebase
       .database()
       .ref('messages/' + this.props.roomId);
@@ -39,7 +51,19 @@ export default class Chat extends Component {
     startListening();
   };
 
+
+  establishColor = (colors) => {
+    let names = colors.names;
+    let randomProperty = function (names) {
+      let keys = Object.keys(names)
+      return names[keys[ keys.length * Math.random() << 0]];
+  };
+    return randomProperty(names)
+  }
+
   render() {
+
+
     return (
       <div id="chat">
         <span id="username" >{this.state.name}</span>
@@ -55,7 +79,7 @@ export default class Chat extends Component {
           .slice(0)
           .reverse()
           .map((message, index) => (
-            <h6 key={index} className={(index % 2 === 0 ? 'color1 message' : 'color2 message')}>
+            <h6 key={index} className={(index % 2 === 0 ? 'color1 message' : 'color2 message')} style={{'backgroundColor': this.state.color}}>
               {message.user}: {message.message}
             </h6>
           ))}
