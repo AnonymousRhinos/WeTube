a75e52c62cddac7daf2afa32b8f25d4c23fb8e62
David Lu <dlu777@gmail.com>
2018-05-08 09:55:03 -0500

    video screen moved

--- src/Components/video/video-screen.js
+++ src/Components/video/video-screen.js
@@ -1,188 +1,67 @@
-import React, { Component } from 'react';
-import YouTube from 'react-youtube';
-import myFirebase from '../../Firebase/firebaseInit';
+// import React, { Component } from 'react';
+// import YouTube from 'react-youtube';
+// import myFirebase from '../../Firebase/firebaseInit';
 
-class Screen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentIndex: 0,
-    };
-    this.player = {};
-    this.isJoining = true;
-  }
+// class Screen extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentIndex: 0,
+//     };
+//     this.player = {};
+//     this.isJoining = true;
+//   }
 
-  componentDidMount = () => {
-    this.listenToFirebase();
-  };
 
-  componentDidUpdate = (prevProps) => {
-    if (prevProps.videoId !== this.props.videoId || prevProps.roomId !== this.props.roomId) {
-      this.stopListening();
-      this.listenToFirebase();
-    }
-  }
 
-  listenToFirebase = () => {
-    let { currentIndex } = this.state;
-    let { playlist, update, roomId, videoId } = this.props
-    this.usersRef = myFirebase.database().ref('users/' + roomId);
-    this.roomRef = myFirebase.database().ref('rooms/' + roomId);
+//   render() {
 
-    let startListeningUsers = () => {
-      this.usersRef.on('child_added', snapshot => {
-        let user = snapshot.val();
-        let currentTime = this.player.getCurrentTime();
-        let playerStatus = this.player.getPlayerState();
-        // console.log('running, currentTime:', currentTime)
-        this.roomRef.set({
-          roomId: roomId,
-          playerStatus,
-          currentTime,
-        });
-      })
-    }
 
-    let startListeningRoom = () => {
-      this.roomRef.on('value', snapshot => {
-        let value = snapshot.val();
-        if (value.playerStatus > -1) {
-          let player = this.player;
-          let status = value.playerStatus;
-          let currentTime = value.currentTime;
-          if (this.isJoining) {
-            // console.log('status', status, 'current time', currentTime)
-            player.seekTo(currentTime);
-            if (status === 1) player.playVideo();
-            else if (status === 2) player.pauseVideo();
-            this.isJoining = false;
-          } else if (status !== player.getPlayerState() || status === 0) {
-            if (status === 1) {
-              player.seekTo(currentTime);
-              player.playVideo();
-            } else if (status === 2) player.pauseVideo();
-            else if (status === 0) {
-              if (currentIndex + 1 < this.props.playlist.length) {
-                currentIndex++;
-                player.loadVideoById(this.props.playlist[currentIndex], 2);
-              }
-              else {
-                currentIndex++;
-                player.stopVideo();
-              }
-            }
-          }
-        }
-      });
-    };
-    startListeningRoom();
+//     return (
 
-    this.videosRef = myFirebase.database().ref('videos/' + roomId);
-    let startListeningVideos = () => {
-      this.videosRef.on('child_added', snapshot => {
-        let video = snapshot.val();
-        console.log('running', video.videoId)
-        update(video.videoId)
-      });
-    };
-    startListeningVideos();
-    myFirebase
-    .database()
-    .ref('videos/' + roomId + '/' + videoId)
-    .set({ videoId });
-  }
-
-  stopListening = () => {
-    this.roomRef.off();
-    this.videosRef.off();
-    this.usersRef.off();
-  }
-
-  handler = event => {
-    this.roomRef.set({
-      roomId: this.props.roomId,
-      playerStatus: event.data,
-      currentTime: event.target.getCurrentTime(),
-    });
-  };
-
-  _onReady = event => {
-    this.player = event.target;
-    console.log(this.player)
-    event.target.pauseVideo();
-  };
-
-  componentWillUnmount = () => {
-    this.stopListening();
-  }
-
-  render() {
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 0,
-        enablejsapi: 1,
-        modestbranding: 1,
-        //origin: ourdomain.com,
-        rel: 0,
-      },
-    };
-
-    return (
-      <YouTube
-        id="vidScreen"
-        videoId={this.props.videoId}
-        opts={opts}
-        onReady={this._onReady}
-        onPlay={this.handler}
-        onPause={this.handler}
-        onEnd={this.handler}
-      />
-    );
-  }
-}
-
-export default Screen;
-
-// // 2. This code loads the IFrame Player API code asynchronously.
-// var tag = document.createElement('script');
-
-// tag.src = 'https://www.youtube.com/iframe_api';
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// // 3. This function creates an <iframe> (and YouTube player)
-// //    after the API code downloads.
-// var player;
-// function onYouTubeIframeAPIReady() {
-//   player = new YT.Player('player', {
-//     height: '390',
-//     width: '640',
-//     videoId: 'M7lc1UVf-VE',
-//     events: {
-//       onReady: onPlayerReady,
-//       onStateChange: onPlayerStateChange,
-//     },
-//   });
-// }
-
-// // 4. The API will call this function when the video player is ready.
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-// }
-
-// // 5. The API calls this function when the player's state changes.
-// //    The function indicates that when playing a video (state=1),
-// //    the player should play for six seconds and then stop.
-// var done = false;
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
+//     );
 //   }
 // }
-// function stopVideo() {
-//   player.stopVideo();
-// }
+
+// export default Screen;
+
+// // // 2. This code loads the IFrame Player API code asynchronously.
+// // var tag = document.createElement('script');
+
+// // tag.src = 'https://www.youtube.com/iframe_api';
+// // var firstScriptTag = document.getElementsByTagName('script')[0];
+// // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // // 3. This function creates an <iframe> (and YouTube player)
+// // //    after the API code downloads.
+// // var player;
+// // function onYouTubeIframeAPIReady() {
+// //   player = new YT.Player('player', {
+// //     height: '390',
+// //     width: '640',
+// //     videoId: 'M7lc1UVf-VE',
+// //     events: {
+// //       onReady: onPlayerReady,
+// //       onStateChange: onPlayerStateChange,
+// //     },
+// //   });
+// // }
+
+// // // 4. The API will call this function when the video player is ready.
+// // function onPlayerReady(event) {
+// //   event.target.playVideo();
+// // }
+
+// // // 5. The API calls this function when the player's state changes.
+// // //    The function indicates that when playing a video (state=1),
+// // //    the player should play for six seconds and then stop.
+// // var done = false;
+// // function onPlayerStateChange(event) {
+// //   if (event.data == YT.PlayerState.PLAYING && !done) {
+// //     setTimeout(stopVideo, 6000);
+// //     done = true;
+// //   }
+// // }
+// // function stopVideo() {
+// //   player.stopVideo();
+// // }
--- src/Components/video/video.js
+++ src/Components/video/video.js
@@ -5,6 +5,8 @@ import { VideoSearch } from '../index.js';
 import { VideoChat } from '../index.js';
 import { Chat } from '../index.js';
 import { VideoShare } from '../index.js';
+import YouTube from 'react-youtube';
+import myFirebase from '../../Firebase/firebaseInit';
 
 class Video extends Component {
   constructor(props) {
@@ -13,33 +15,156 @@ class Video extends Component {
       videoId: this.props.match.params.id.split('&')[1],
       roomId: this.props.match.params.id,
       playlist: [],
+      currentIndex: 0,
     };
+    this.player = {};
+    this.isJoining = true;
+  }
+
+  componentDidMount = () => {
+    this.listenToFirebase();
+  };
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.videoId !== this.props.videoId || prevProps.roomId !== this.props.roomId) {
+      this.stopListening();
+      this.listenToFirebase();
+    }
+  }
+
+  listenToFirebase = () => {
+    let { currentIndex, playlist, update, roomId, videoId } = this.state;
+    this.usersRef = myFirebase.database().ref('users/' + roomId);
+    this.roomRef = myFirebase.database().ref('rooms/' + roomId);
+
+    let startListeningUsers = () => {
+      this.usersRef.on('child_added', snapshot => {
+        let user = snapshot.val();
+        let currentTime = this.player.getCurrentTime();
+        let playerStatus = this.player.getPlayerState();
+        // console.log('running, currentTime:', currentTime)
+        this.roomRef.set({
+          roomId: roomId,
+          playerStatus,
+          currentTime,
+        });
+      })
+    }
+
+    let startListeningRoom = () => {
+      this.roomRef.on('value', snapshot => {
+        let value = snapshot.val();
+        if (value.playerStatus > -1) {
+          let player = this.player;
+          let status = value.playerStatus;
+          let currentTime = value.currentTime;
+          if (this.isJoining) {
+            // console.log('status', status, 'current time', currentTime)
+            player.seekTo(currentTime);
+            if (status === 1) player.playVideo();
+            else if (status === 2) player.pauseVideo();
+            this.isJoining = false;
+          } else if (status !== player.getPlayerState() || status === 0) {
+            if (status === 1) {
+              player.seekTo(currentTime);
+              player.playVideo();
+            } else if (status === 2) player.pauseVideo();
+            else if (status === 0) {
+              if (currentIndex + 1 < this.state.playlist.length) {
+                currentIndex++;
+                player.loadVideoById(this.state.playlist[currentIndex], 2);
+              }
+              else {
+                currentIndex++;
+                player.stopVideo();
+              }
+            }
+          }
+        }
+      });
+    };
+    startListeningRoom();
+
+    this.videosRef = myFirebase.database().ref('videos/' + roomId);
+    let startListeningVideos = () => {
+      this.videosRef.on('child_added', snapshot => {
+        let video = snapshot.val();
+        console.log('running', video.videoId)
+        this.updatePlaylist(video.videoId)
+      });
+    };
+    startListeningVideos();
+    myFirebase
+      .database()
+      .ref('videos/' + roomId + '/' + videoId)
+      .set({ videoId });
+  }
+
+  stopListening = () => {
+    this.roomRef.off();
+    this.videosRef.off();
+    this.usersRef.off();
+  }
+
+  handler = event => {
+    this.roomRef.set({
+      roomId: this.state.roomId,
+      playerStatus: event.data,
+      currentTime: event.target.getCurrentTime(),
+    });
+  };
+
+  _onReady = event => {
+    this.player = event.target;
+    console.log(this.player)
+    event.target.pauseVideo();
+  };
+
+  componentWillUnmount = () => {
+    this.stopListening();
   }
 
   updatePlaylist = newVideo => {
     const { playlist } = this.state;
-    this.setState(prevState => ({playlist: [...prevState.playlist, newVideo]}))
+    this.setState(prevState => ({ playlist: [...prevState.playlist, newVideo] }))
   }
 
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0,
+        enablejsapi: 1,
+        modestbranding: 1,
+        //origin: ourdomain.com,
+        rel: 0,
+      },
+    };
+
     return (
       <div className="vid-view">
-        <VideoShare roomId={this.state.roomId}/>
+        <VideoShare roomId={this.state.roomId} />
         <div id="video">
           <div id="screen">
-            <Screen videoId={this.state.videoId}
-                    roomId={this.state.roomId}
-                    playlist={this.state.playlist}
-                    update={this.updatePlaylist}
-                    />
+            <YouTube
+              id="vidScreen"
+              videoId={this.state.videoId}
+              opts={opts}
+              onReady={this._onReady}
+              onPlay={this.handler}
+              onPause={this.handler}
+              onEnd={this.handler}
+            />
             <VideoSearch roomId={this.state.roomId} />
           </div>
-          <Chat videoId={this.state.videoId} roomId={this.state.roomId} />
+          <Chat player={this.player} videoId={this.state.videoId} roomId={this.state.roomId} />
         </div>
         <Queue videoId={this.state.videoId}
-               roomId={this.state.roomId}
-               playlist={this.state.playlist}
-               />
+          roomId={this.state.roomId}
+          playlist={this.state.playlist}
+        />
       </div>
     );
   }
