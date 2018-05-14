6822bd9514accc89847d1ce278d5eb5d9277ff6a
Jaime Syvino <jsyvino@gmail.com>
2018-05-14 15:51:16 -0500

    bext I can do with user logins

--- src/Components/login.js
+++ src/Components/login.js
@@ -45,7 +45,6 @@ class Login extends Component {
     }
 
     render() {
-        console.log(firebase.auth().currentUser)
         if (!this.state.isSignedIn) {
             return (
                 <div>
