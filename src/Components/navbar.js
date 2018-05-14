3cb8466ae71fcf7e7095301030e3ace2f63e88a7
nangelos <nicksangelos@gmail.com>
2018-05-14 10:40:02 -0500

    changes to copy url and playlist thumbnails

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -12,19 +12,25 @@ class MyNavbar extends Component {
   render() {
     return (
       <header className="navbar">
-          <section className="navbar-section">
+        <section className="navbar-section">
           <Link to={'/home'}>
-          <img className="logo-navbar" src="/logo.jpg" alt="logo"/>
+            <img className="logo-navbar" src="/logo.jpg" alt="logo" />
           </Link>
           <Link to={'/home'} className="btn-link" id="nav-home">
             WeTube
           </Link>
         </section>
-      <section className="navbar-section">	
-          <Link to="#" className="btn-link">	
-            A Link	
-          </Link>	
-          <button onClick={this.addToClipboard} className="url-btn">Copy URL</button>	
+        <section className="navbar-section">
+          <Link to="#" className="btn-link">
+            A Link
+          </Link>
+          <div>
+            <a href="#" onClick={this.addToClipboard}>
+              <img src="/images/copy-url.png" alt="copy-url" id="copy-url-img" />
+            </a>
+            <button
+              onClick={this.addToClipboard} className="url-btn">Copy URL</button>
+          </div>
         </section>
       </header>
     );
