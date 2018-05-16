9a4bf098f83c975009465b5691652c842d695abd
nangelos <nicksangelos@gmail.com>
2018-05-16 11:44:27 -0500

    conditional rendering of navbar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -8,24 +8,44 @@ import YouTube from './navbar-youtube';
 class MyNavbar extends Component {
 
   render() {
-    return (
-      <header className="navbar">
-        <section className="navbar-section">
-          <Link to={'/home'}>
-            <img className="logo-navbar" src="/logo.jpg" alt="logo" />
-          </Link>
-          <Link to={'/home'} className="btn-link" id="nav-home">
-            WeTube
-          </Link>
-        </section>
-        <section className="navbar-section">
-          <YouTube />
-          <OnlineUserList roomId={window.location.pathname.slice(6)} userName={this.props.userName} />
-          <CopyUrl />
-          <Login setUser={this.props.setUser} />
-        </section>
-      </header>
-    );
+    if (window.location.href.includes('room/')) {
+      return (
+        <header className="navbar">
+          <section className="navbar-section">
+            <Link to={'/home'}>
+              <img className="logo-navbar" src="/logo.jpg" alt="logo" />
+            </Link>
+            <Link to={'/home'} className="btn-link" id="nav-home">
+              WeTube
+            </Link>
+          </section>
+          <section className="navbar-section">
+            <YouTube />
+            <OnlineUserList roomId={window.location.pathname.slice(6)} userName={this.props.userName} />
+            <CopyUrl />
+            <Login setUser={this.props.setUser} />
+          </section>
+        </header>
+      );
+    }
+    else {
+      return (
+        <header className="navbar">
+          <section className="navbar-section">
+            <Link to={'/home'}>
+              <img className="logo-navbar" src="/logo.jpg" alt="logo" />
+            </Link>
+            <Link to={'/home'} className="btn-link" id="nav-home">
+              WeTube
+            </Link>
+          </section>
+          <section className="navbar-section">
+            <YouTube />
+            <Login setUser={this.props.setUser} />
+          </section>
+        </header>
+      );
+    }
   }
 }
 
